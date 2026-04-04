0890b374f3929aad8fbdf87b5cd4f631181e4ae2
latif <mabdullatifm1127@gmail.com>
2026-04-04 23:26:13 +0700

    first commit

---
+++ src/pages/produk/server.tsx
@@ -0,0 +1,31 @@
+import TampilanProduk from "../../views/produk";
+
+
+type ProductType = {
+    id: string;
+    name: string;
+    price: number;
+    image: string;
+    category: string;
+};
+const halamanProdukServer = ({products}: {products: ProductType[]}) => {
+    return (
+        <div>
+            <h1>Halaman Produk Server by Abdul Latif</h1>
+            <TampilanProduk products={products} />
+        </div>
+    );
+}
+
+export default halamanProdukServer;
+
+export async function getServerSideProps() {
+    const res = await fetch("http://localhost:3000/api/produk");
+    const respone = await res.json();
+
+    return {
+        props: {
+            products: respone
+        },
+    };
+}
